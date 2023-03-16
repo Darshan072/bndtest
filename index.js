ddeceb60a0e93315b87116c8b7f8786aabc9cd70
Darshan Ghetiya <30925505+Darshan072@users.noreply.github.com>
2023-03-17 03:09:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/health", (req, res) => {
-    res.send("Kashi");
+    res.send("Kashi11111");
 });
 app.listen("6000", () => {
     console.log("listening 6000");

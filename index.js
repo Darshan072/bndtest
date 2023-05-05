02157f087a933d9379233d11c39d5c85842311f9
darshan074 <er.darshanghetiya@gmail.com>
2023-05-05 14:22:05 +0530

    new commit for push

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 
 app.get("/health", (req, res) => {
-    res.send("DARSHAN 999 Ghetiya");
+    res.send("DARSHAN 999 1Ghetiya");
 });
 app.listen("6000", () => {
     console.log("listening 6000");

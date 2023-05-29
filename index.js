6fe7549e2674788124097000b4993a328f52648d
darshan074 <er.darshanghetiya@gmail.com>
2023-05-29 19:58:34 +0530

    new changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 
 app.get("/health", (req, res) => {
-    res.send("DARSHAN 1000 Ghetiya");
+    res.send("DARSHAN 10001 Ghetiya");
 });
 app.listen("6000", () => {
     console.log("listening 6000");
